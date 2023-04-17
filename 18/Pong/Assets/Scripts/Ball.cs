cd35768d61408e57c8fbdd5165bc5ecd01707009
i_jemin <jemin.lee@unity3d.com>
2023-04-18 00:34:26 +0900

    18장-19장 완성 프로젝트 중간 저장

---
+++ 18/Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class Ball : NetworkBehaviour
+{
+    private Vector2 direction;
+    private readonly float speed = 10f;
+
+    public override void OnNetworkSpawn()
+    {
+        direction = Vector2.left + Random.insideUnitCircle;
+        direction = direction.normalized;
+    }
+    
+    private void FixedUpdate()
+    {
+        if (!IsServer || !GameManager.Instance.IsGameActive)
+        {
+            return;
+        }
+
+        var distance = speed * Time.deltaTime;
+        var hit = Physics2D.Raycast(transform.position, direction, distance);
+
+        if (hit.collider != null)
+        {
+            var goalpost = hit.collider.GetComponent<Goalpost>();
+            if (goalpost)
+            {
+                goalpost.OnGoal();
+                Respawn();
+                return;
+            }
+
+            direction = Vector2.Reflect(direction, hit.normal);
+            direction += Random.insideUnitCircle * 0.01f; // 조금 랜덤하게 튕기게
+            direction = direction.normalized;
+        }
+
+        transform.position = (Vector2) transform.position + direction * distance;
+    }
+    
+    private void Respawn()
+    {
+        transform.position = Vector3.zero;
+        direction = Random.insideUnitCircle.normalized;
+    }
+}
---
+++ 18/Pong/Assets/Scripts/Goalpost.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class Goalpost : MonoBehaviour
+{
+    public ulong opponentId;
+    
+    public void OnGoal()
+    {
+        if (!NetworkManager.Singleton.IsHost)
+        {
+            return;
+        }
+        
+        GameManager.Instance.AddScore(opponentId, 1);
+    }
+}
